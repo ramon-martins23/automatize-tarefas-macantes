d3728cd3171c97a420db5a1e28cfb80fc1d8b06a
amnor0 <amnor1307@gmail.com>
2023-10-16 07:59:33 -0300

    usando listas em magic8Ball

--- magic8Ball/magic8Ball02.py
+++ magic8Ball/magic8Ball02.py
@@ -1,30 +1,18 @@
 import random
 
+messages = ['It is certain',
+    'It is decidedly so',
+    'Yes definitely',
+    'Reply hazy try again',
+    'Ask again later',
+    'Concentrate and ask again',
+    'My reply is no',
+    'Outlook not so good',
+    'Very doubtful']
 
-def getAnswer(answerNumber): #dá uma resposta aleatória com base num número também aleatório
-    if answerNumber == 1:
-        return 'It is certain'
-    elif answerNumber == 2:
-        return 'It is decidedly so'
-    elif answerNumber == 3:
-        return 'Yes'
-    elif answerNumber == 4:
-        return 'Reply hazy try again'
-    elif answerNumber == 5:
-        return 'Ask again later'
-    elif answerNumber == 6:
-        return 'Concentrate and ask again'
-    elif answerNumber == 7:
-        return 'My reply is no'
-    elif answerNumber == 8:
-        return 'Outlook not so good'
-    elif answerNumber == 9:
-        return 'Very doubtful'
-    
 while True:
-    r = input('Faça uma pergunta á bola mágica: ')
-    if r == '': #se o user não digitar nada, ele pede novamente
+    userQuestion = input('Faça uma pergunta à bola mágica: ')
+    if userQuestion != '':
+        print(messages[random.randint(0,len(messages)-1)])
+    else:
         continue
-    else: #se o user digitar algo, ele da a resposta aleatória
-        print(getAnswer(random.randint(1,9))) #aqui é gerado o número aleatório e passado como parametro para a funcão
-        
